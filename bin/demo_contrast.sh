eaad8f66e2b898452e54d374aeba0d4f04ece3d2
Robert Statsinger <robert.statsinger@contrastsecurity.com>
2021-02-05 08:20:55 -0800

    Update demo_contrast.sh

--- bin/demo_contrast.sh
+++ bin/demo_contrast.sh
@@ -5,7 +5,7 @@
 # Variables
 
 # This value should updated whenever a new AMI for the Contrast HDE1 "golden image" is created
-DEFAULT_DEMO_AMI=hde-0.1.55
+DEFAULT_DEMO_AMI=hde-0.1.55b
 
 USAGE="Usage: $0 [demo version] [customer name or description] [your name] [your target AWS region] [hours to keep demo running]\n\nExample:\n$0 default 'Acme Corp' 'Brian Chau' us-west-1 2"
 VERSION=$1
